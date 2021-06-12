8a806c831728649fc6e3f12eb2849751ff6662fc
Jbullis29 <jsbullis16@gmail.com>
2021-06-12 13:59:00 -0600

    oh my lanta i finally got the freking table to work

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -1,10 +1,17 @@
 // from data.js
 var tableData = data;
 
-data.forEach(function(ufoSightings) {
+data.forEach((ufoSightings) => {
     console.log(ufoSightings);
-})
 
-data.forEach(function(ufoSightings){
-    var row = tbody.append('tr');
-})
+var table = d3.select('tbody')
+
+   var row = table.append("tr");
+   Object.entries(ufoSightings).forEach(function([key, value]) {
+        console.log(key, value);
+
+        var cell = row.append('td');
+        cell.text(value);
+
+    });
+});
