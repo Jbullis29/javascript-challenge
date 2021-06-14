5cec9817c612d23692eb3d3695329386aea42e3d
Jbullis29 <jsbullis16@gmail.com>
2021-06-14 17:07:12 -0600

    Making progress got date time figured out

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -1,41 +1,35 @@
 // from data.js
 var tableData = data;
+//console.log(tableData);
+
 
 data.forEach((ufoSightings) => {
-    console.log(ufoSightings);
-
+    //console.log(ufoSightings)
 var table = d3.select('tbody')
-
-   var row = table.append("tr");
-   Object.entries(ufoSightings).forEach(function([key, value]) {
-        console.log(key, value);
-
-        var cell = row.append('td');
-        cell.text(value);
-
-    });
-});
-
-var button = d3.select('#button');
-var form = d3.select('#form-control');
-
-button.on('click', runFilter);
-form.on('submit', runFilter);
-
-function runFilter() {
-d3.event.preventDefault();
-var inputdate = d3.select('datetime');
-var inputValue = inputdate.property('value');
-    console.log(inputValue);
-console.log(tableData);
-var filteredData = tableData.filter(sighting => sighting.date >= inputValue);
-console.log(filteredData);
-
-Object.entries(filteredData).forEach(function([key, value]) {
-    console.log(key, value);
-
+    var row = table.append("tr");
+    Object.entries(ufoSightings).forEach(function([key, value]) {
+        const convertDate = Date.parse(ufoSightings.datetime);
+        console.log(convertDate)
     var cell = row.append('td');
     cell.text(value);
     });
+});
+
+var button = document.getElementById('filter-btn');
+var form = document.getElementById('form');
+
+
+button.addEventListener('click', runFilter);
+form.addEventListener('submit', runFilter)
+
+function runFilter(){
+    event.preventDefault();
+    console.log('its alive');
+    var inputElement = d3.select('input');
+    var userDate = inputElement.property('value');
+    const convertUserDate = Date.parse(userDate);
+    console.log(convertUserDate)
+   
 }
+  
 
