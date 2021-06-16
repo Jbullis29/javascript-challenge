3a900492ca5fc76cfd5a3d96cd9d64aa3dbcc5d5
Jbullis29 <jsbullis16@gmail.com>
2021-06-16 14:34:00 -0600

    Almost got my filter to work

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -6,30 +6,51 @@ var tableData = data;
 data.forEach((ufoSightings) => {
     //console.log(ufoSightings)
 var table = d3.select('tbody')
-    var row = table.append("tr");
+ var row = table.append("tr");
     Object.entries(ufoSightings).forEach(function([key, value]) {
-        const convertDate = Date.parse(ufoSightings.datetime);
-        console.log(convertDate)
     var cell = row.append('td');
     cell.text(value);
-    });
+    }); 
 });
-
+    
 var button = document.getElementById('filter-btn');
 var form = document.getElementById('form');
 
 
-button.addEventListener('click', runFilter);
-form.addEventListener('submit', runFilter)
-
-function runFilter(){
+    button.addEventListener('click', runFilter);
+    form.addEventListener('submit', runFilter)
+    
+    function runFilter(){
     event.preventDefault();
-    console.log('its alive');
+    var table = d3.select('tbody');
+
+    table.html('');
+    const start_end_dates =[]
     var inputElement = d3.select('input');
     var userDate = inputElement.property('value');
-    const convertUserDate = Date.parse(userDate);
-    console.log(convertUserDate)
-   
-}
-  
-
+    var splitDates = userDate.split(',');
+        splitDates.forEach((date)=>{
+         start_end_dates.push(Date.parse(date));
+        });
+    var startDate = start_end_dates[0]
+    var endDate = start_end_dates[1]
+    filterList=[]
+    data.forEach((sighting)=>{
+        var row = table.append("tr");
+        Object.entries(sighting).forEach(function([key, value]){
+            const convertDate = Date.parse(sighting.datetime);
+            if(startDate <= convertDate){
+            var cell = row.append('td');
+            cell.text(value);
+            console.log('gottem')}
+        });
+        data.forEach((sighting)=>{
+            var row = table.append("tr");
+            Object.entries(sighting).forEach(function([key, value]){
+                const convertDate = Date.parse(sighting.datetime);
+                if(endDate >= convertDate){
+                var cell = row.append('');
+                cell.text(value);
+                console.log('yuurp')}
+            });
+        })
