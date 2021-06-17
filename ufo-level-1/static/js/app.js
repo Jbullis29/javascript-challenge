b8530e3afa142b2d8f688b201f26fb349e38764e
Jbullis29 <jsbullis16@gmail.com>
2021-06-16 18:14:20 -0600

    seperated my search app into ufo-level2

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -12,39 +12,3 @@ var table = d3.select('tbody')
     cell.text(value);
     }); 
 });
-    
-var button = document.getElementById('filter-btn');
-var form = document.getElementById('form');
-
-
-    button.addEventListener('click', runFilter);
-    form.addEventListener('submit', runFilter)
-    
-    function runFilter(){
-    event.preventDefault();
-    var table = d3.select('tbody');
-
-    table.html('');
-    const start_end_dates =[]
-    var inputElement = d3.select('input');
-    var userDate = inputElement.property('value');
-    var splitDates = userDate.split(',');
-        splitDates.forEach((date)=>{
-         start_end_dates.push(Date.parse(date));
-        });
-    var startDate = start_end_dates[0]
-    var endDate = start_end_dates[1]
-    filterList=[]
-    data.forEach((sighting)=>{
-        var row = table.append("tr");
-        Object.entries(sighting).forEach(function([key, value]){
-            const convertDate = Date.parse(sighting.datetime);
-            if(startDate <= convertDate){
-            var cell = row.append('td');
-            cell.text(value);
-            console.log('gottem')}
-            else if(endDate > convertDate){}
-        
-        });
-});
-    }
