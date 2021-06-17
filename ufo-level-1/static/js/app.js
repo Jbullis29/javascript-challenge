1da0d9cabc3d77ef5ca9687785bc65c98db8465a
Jbullis29 <jsbullis16@gmail.com>
2021-06-17 12:10:03 -0600

    date filter range statement is working!

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -2,13 +2,18 @@
 var tableData = data;
 //console.log(tableData);
 
-
+// for loop to loop through all my data
 data.forEach((ufoSightings) => {
     //console.log(ufoSightings)
+    //selecting the table elemant to store my data
 var table = d3.select('tbody')
+//adding a row to my data for each data entry
  var row = table.append("tr");
+ //looping through all my data points and pulling the key and value from it
     Object.entries(ufoSightings).forEach(function([key, value]) {
+    // adding new Table Dat column to ad data to 
     var cell = row.append('td');
+    //Adding my value to the table as text
     cell.text(value);
     }); 
 });
